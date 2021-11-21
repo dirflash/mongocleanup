a1e47e86b8055c40ff1aef90c74573cdc9c61253
Aaron Davis <aaron.e.davis@gmail.com>
2021-11-20 21:08:23 -0700

    Fixed console log message

--- mongo.py
+++ mongo.py
@@ -114,6 +114,6 @@ if __name__ == "__main__":
             sleep(300)
             countdwn = ennext - datetime.now()
             console.log(
-                "--- Next run in t-minus: [bold cyan]%.1f[/bold cyan] --- % (countdwn)"
+                "--- Next run in t-minus: [bold cyan]%.1f[/bold cyan] ---" % countdwn
             )
             sleep(1500)
